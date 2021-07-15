9433e098980e169a45dab3c7ee73f63cde1cb40c
Dilan Isuru Jayasinghe <59989533+DilaN97514@users.noreply.github.com>
2021-07-16 01:28:11 +0530

    Create Splash

---
+++ PharmacyManagement/PharmacyManagement/Splash.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace PharmacyManagement
+{
+    partial class Splash
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Splash));
+            this.progressBar = new Bunifu.Framework.UI.BunifuCircleProgressbar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.animated = false;
+            this.progressBar.animationIterval = 5;
+            this.progressBar.animationSpeed = 300;
+            this.progressBar.BackColor = System.Drawing.Color.Transparent;
+            this.progressBar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("progressBar.BackgroundImage")));
+            this.progressBar.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F);
+            this.progressBar.ForeColor = System.Drawing.Color.White;
+            this.progressBar.LabelVisible = true;
+            this.progressBar.LineProgressThickness = 8;
+            this.progressBar.LineThickness = 5;
+            this.progressBar.Location = new System.Drawing.Point(283, 177);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.progressBar.MaxValue = 100;
+            this.progressBar.Name = "progressBar";
+            this.progressBar.ProgressBackColor = System.Drawing.Color.Gainsboro;
+            this.progressBar.ProgressColor = System.Drawing.Color.White;
+            this.progressBar.Size = new System.Drawing.Size(221, 221);
+            this.progressBar.TabIndex = 0;
+            this.progressBar.Value = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 40.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(148, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(514, 78);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "PharmaCenter";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(262, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(282, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Powered by Dilan Isuru";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(655, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Ver 1.0";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Splash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Brown;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Splash";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Splash_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuCircleProgressbar progressBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
